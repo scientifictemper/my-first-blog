ba6c27b096385a255b250e19493192d4d2cf890e
admin <scientifictemper94@gmail.com>
2020-04-08 12:13:40 +0530

    changed the html for site

--- web/web/urls.py
+++ web/web/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.urls import path,include
 from django.urls import path
+from django import views
 
 urlpatterns = [
+    path('', views.post_list, name='post_list'),
+
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
+
 ]
